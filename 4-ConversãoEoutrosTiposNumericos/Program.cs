6ec1000915d412f5fd5e8ea7c829933fd597b665
guilherme.artigiani <guilherme.artigiani@008063NW10ZPK2>
2022-03-03 15:26:42 -0300

    Adicionar arquivos de projeto.

---
+++ 4-ConversãoEoutrosTiposNumericos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversãoEoutrosTiposNumericos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Executando projeto 4- Conversão e outros tipos numéricos");
+
+            double salario;
+            salario = 1500.50;
+
+            int salarioEminteiro;
+            salarioEminteiro = (int)salario;
+            
+            Console.WriteLine(salarioEminteiro);
+
+            Console.WriteLine("Projeto Finalizado, Aperte ENTER para finalizar...");
+            Console.ReadLine();
+
+        }
+    }
+}
