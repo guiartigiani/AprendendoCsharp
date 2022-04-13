2074d51dd674b99052bb4eebe5603a638ff92420
Guilherme Artigiani <82193572+guiartigiani@users.noreply.github.com>
2022-04-13 13:31:38 -0300

    Add files via upload

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,8 @@
+using System;
+namespace Calculadora
+{
+    public static void Main(string[] args)
+    {
+        
+    } 
+}
---
+++ Calculadora/Calculadora/Calculo.cs
@@ -0,0 +1,49 @@
+﻿
+public class Calculo
+{
+    int num1, num2;
+    char menu;
+    public int Somar(int num1, int num2)
+    {
+        int result = num1 + num2;
+        return result;
+    }
+    public int Multiplicar(int num1, int num2)
+    {
+        int result = num1 * num2;
+        return result;
+    }
+    public int Subtrair(int num1, int num2)
+    {
+        int result = num1 - num2;
+        return result;
+    }
+    public int Dividir(int num1, int num2)
+    {
+        int result = num1 / num2;
+        return result;
+    }
+
+    public void escolha()
+    {
+        switch (menu)
+        {
+            case '1':
+                Somar(num1, num2);
+                break;
+            case '2':
+                Subtrair(num1, num2);
+                break;
+            case '3':
+                Dividir(num1, num2);
+                break;
+            case '4':
+                Multiplicar(num1, num2);
+                break;
+            default:
+                System.Environment.Exit(-1);
+                break;
+
+        }
+    }
+}
