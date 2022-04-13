031f8ca09f4e4e039a53d436f6a4e12c39f357b1
Guilherme Artigiani <82193572+guiartigiani@users.noreply.github.com>
2022-04-13 13:33:45 -0300

    Add files via upload

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Calculadora
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero:");
+            int valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro numero:");
+            int valor2 = int.Parse(Console.ReadLine());
+           
+            Calculo operacao = new Calculo();
+            operacao.escolha();
+            
+            
+        }
+    }
+}
